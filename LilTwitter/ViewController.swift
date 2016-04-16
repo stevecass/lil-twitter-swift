670f66c3bc517d3bbaf3996d6da1ab662c0e4261
Steven Cassidy <steven.cassidy@gmail.com>
2016-04-16 10:11:28 -0400

    Add a tableview that shows fake data

--- LilTwitter/ViewController.swift
+++ LilTwitter/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+  let tweets = ["aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii", "jjj", "kkk", "lll", "mmm"]
 
   @IBOutlet weak var tfTweetText: UITextField!
+  @IBOutlet weak var tableView: UITableView!
 
   @IBAction func btnSendClicked(sender: AnyObject) {
     let tweet = tfTweetText.text
     print("I got clicked. Tweet text is \(tweet)")
+  }
+
+  // UITableViewDataSource
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tweets.count
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+    cell.textLabel?.text = tweets[indexPath.row]
+    return cell
   }
 
   override func viewDidLoad() {
