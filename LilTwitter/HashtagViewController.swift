a03e44019d779a048fe67f31f575de554000107e
Steven Cassidy <steven.cassidy@gmail.com>
2016-04-17 19:21:30 -0400

    Add a HashtagViewController

---
+++ LilTwitter/HashtagViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HashtagViewController.swift
+//  LilTwitter
+//
+//  Created by Steven Cassidy on 4/17/16.
+//  Copyright © 2016 Steven Cassidy. All rights reserved.
+//
+
+import UIKit
+
+class HashtagViewController : UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("Loaded \(NSStringFromClass(self.dynamicType))")
+  }
+
+
+}
