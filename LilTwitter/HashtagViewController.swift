829476511f728dc5f004b9fde57c0364f554bc75
Steven Cassidy <steven.cassidy@gmail.com>
2016-04-17 19:43:00 -0400

    Add popular hashtags display

--- LilTwitter/HashtagViewController.swift
+++ LilTwitter/HashtagViewController.swift
@@ -7,13 +7,40 @@
 //
 
 import UIKit
-
+import Alamofire
+import SwiftyJSON
 class HashtagViewController : UITableViewController {
+
+  var tags: [String] = []
+
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tags.count
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("htCell", forIndexPath: indexPath)
+    cell.textLabel?.text = tags[indexPath.row]
+    return cell
+  }
+
 
   override func viewDidLoad() {
     super.viewDidLoad()
     print("Loaded \(NSStringFromClass(self.dynamicType))")
+    loadHashtags()
   }
 
+  private func loadHashtags() {
+    Alamofire.request(.GET, "http://localhost:3000/hashtags/popular").responseJSON { (responseData) -> Void in
+      if((responseData.result.value) != nil) {
+        let json = JSON(responseData.result.value!)
+        if let data = json.arrayObject {
+          self.tags = data.map { JSON($0)["name"].string! }
+          dispatch_async(dispatch_get_main_queue(), {self.tableView.reloadData() })
+        }
+      }
+    }
+  }
 
 }
